f5b52b8dcae0238645322cd60a08996f1005d501
Adam Noel <adamnoel@Adams-MacBook-Pro.local>
2016-07-11 22:10:33 -0230

    Second commit adding a client for testing the server component and began working on the project.

--- ChainLink/HashFunctioon/SHA1HashFunction.cs
+++ ChainLink/HashFunctioon/SHA1HashFunction.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace DHTSharp
 {
 	public class SHA1HashFunction : IHashFunction
 	{
 		public String GetHash(String key) {
-			return SHA1 (key);
+			var sha1 = SHA1.Create();
+			return ConvertHashToString(sha1.ComputeHash(Encoding.UTF8.GetBytes(key)));
+		}
+
+		private String ConvertHashToString(byte[] hashResult)
+		{
+			String hashString = String.Empty;
+
+			for (int i = 0; i < hashResult.Length; i++)
+			{
+				hashString = hashString + hashResult.ToString();
+			}
+
+			return hashString;
 		}
 
 		public int GetRange() {
